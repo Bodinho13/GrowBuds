c82401d2bf0d1771dd49758c665cada43bacf37a
Marcus Bode <bodemarcus13@gmail.com>
2026-08-11 11:43:46 +0200

    add create & update functions for SQLite + methods for formating data from db to production and vice versa

---
+++ services/plants/plantSql.ts
@@ -0,0 +1,30 @@
+
+
+export const getAllPlantsSql = `
+    SELECT * FROM plants
+`;
+
+export const getPlantByIDSql = `
+    SELECT * FROM plants WHERE id = ?
+`;
+
+export const createPlantSql = `
+    INSERT INTO plants (
+        id,
+        name,
+        strain,
+        cross,
+        breeder,
+        notes,
+        createdAt,
+        updatedAt,
+        isArchived
+    )
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+`;
+
+export const updatePlantSql = `
+    UPDATE plants 
+        SET name = ?, strain = ?, cross = ?, breeder = ?, notes = ?, createdAt = ?, updatedAt = ?, isArchived = ?
+    WHERE id = ?
+`;
--- services/plants/types.ts
+++ services/plants/types.ts
@@ -1,1 +1,13 @@
 export type PlantId = string;
+
+export type PlantRow = {
+    id: string;
+    name: string;
+    strain: string | null;
+    cross: string | null;
+    breeder: string | null;
+    notes: string | null;
+    createdAt: string;
+    updatedAt: string;
+    isArchived: number;
+};
