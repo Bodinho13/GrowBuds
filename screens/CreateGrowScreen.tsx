1382b636d85d0f7699f7e973685edc1aa0f76a34
Marcus Bode <bodemarcus13@gmail.com>
2026-08-13 19:56:06 +0200

    add first version of growscreen

---
+++ screens/CreateGrowScreen.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function CreateGrowScreen() {
+    return (
+        <View>
+            <Text>Neuer Grow</Text>
+        </View>
+    );
+}
---
+++ screens/GrowDetailScreen.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function GrowDetailScreen() {
+    return(
+        <View>
+            <Text>Grow Detail</Text>
+        </View>
+    );
+}
