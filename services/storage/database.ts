9ce1af02be18f31614f9664f88dc3234527c15d2
Marcus Bode <bodemarcus13@gmail.com>
2026-08-19 18:07:28 +0200

    add migration function for altering db tables

--- services/storage/database.ts
+++ services/storage/database.ts
@@ -1,14 +1,17 @@
 import * as SQLite from "expo-sqlite";
 import { createTablesSql } from "./databaseSchema";
+import { migrateDatabase } from "./migrations";
 
 export async function getDatabase(): Promise<SQLite.SQLiteDatabase> {
     const db = await SQLite.openDatabaseAsync("growbuds.db");
     await db.execAsync(`PRAGMA foreign_keys= ON;`);
     await db.execAsync(createTablesSql);
+    await migrateDatabase(db);
+
     return db;
 }
 
 export async function initializeDatabase(): Promise<void> {
-    const db = await getDatabase();
+    await getDatabase();
     console.log("SQLite database initialized");
 }
